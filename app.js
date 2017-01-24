2730b7e3598a71b35a1882892f8a6c8735041bdd
Raditya <arya.raditya26@gmail.com>
2017-01-24 20:54:55 +0700

    done

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ let express = require ('express')
 let app = express()
 let router = require('./routes/user')
 let path = require('path');
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', router)
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
--- routes/user.js
+++ routes/user.js
@@ -8,15 +8,41 @@ let user = model.User
 // GET
 router.get('/', function (request, response) {
   user.findAll().then(function (data) {
-    response.render('user', {title: "ini tabel", result: data})
+    response.render('user', {title: "user list", result: data})
   })
 })
 
-
 //POST
+router.post('/',function(req,res){
+  user.create({firstname:req.body.firstname,lastname:req.body.lastname,phone:req.body.phone}).then(function(tes){
+    res.redirect('/')
+  })
+});
 
 //PUT
+router.get('/update/:id', function (req,res) {
+  let id = req.params.id
+  user.findById(id).then(function (data) {
+    res.render('update',{update: data})
+  })
+})
+
+router.post('/updated',function(req,res){
+  let id = req.body.id
+  user.findById(id).then(function (data){
+    if(data){
+      data.updateAttributes({firstname:req.body.firstname,lastname:req.body.lastname,phone:req.body.phone})
+    }
+    res.redirect('/')
+    })
+  })
 
 // DELETE
-
+router.get('/delete/:id',function(req,res){
+  user.findById(req.params.id).then(function(use){
+    use.destroy().then(function(){
+      res.redirect('/')
+    })
+  })
+})
 module.exports = router
