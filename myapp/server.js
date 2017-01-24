81e9e82d85c1f8a512921ada164808ba76556ce8
timocodex <timothy.og.siahaan@gmail.com>
2017-01-24 16:10:33 +0700

    finish sudah di cek

--- myapp/server.js
+++ myapp/server.js
@@ -2,11 +2,19 @@
 
 const express = require('express')
 const router = require('./routes/index')
+const path = require('path');
+const bodyParser = require('body-parser');
+
 let app = express()
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/',router)
+
+
 
 app.listen(3000)
