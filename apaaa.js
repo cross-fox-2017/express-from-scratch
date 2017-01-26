ff0c11977f25ea592cbd094457b2511a1c52a0f7
yomaswn <yaumashf@gmail.com>
2017-01-26 10:19:31 +0700

    beres scratch

--- apaaa.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-let apaindexxx = express()
-const routeaja = require('./routes/index');
-
-apaindexxx.use('/',routeaja)
-apaindexxx.listen(1210)
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,38 @@
 const express = require('express');
- let apaindex = express.Router()
+let router = express.Router();
+const models = require('../models');
+var user = models.User;
 
-apaindex.get('/', function (req,res){
-  res.send('================H8')
-})
+router.get('/', function(req, res) {
+  user.findAll().then(function(result){
+    res.render('index', {result: result, title: "UserList"})
+  })
+});
 
-module.exports = apaindex
+router.post('/create', function(req, res) {
+  user.create({firstname: req.body.firstname, lastname: req.body.lastname, phone: req.body.phone, createdAt: new Date(), updatedAt: new Date()}).then(function(result){
+    res.redirect('/')
+  })
+});
+
+router.get('/delete/:id',function(req,res){
+  user.findById(req.params.id).then(function(use){
+    use.destroy()
+    res.redirect('/')
+    })
+});
+
+router.get('/update/:id',function(req,res){
+  user.findById(req.params.id).then(function(use){
+    res.render('update',{list:use})
+    })
+});
+
+router.post('/updated',function(req,res){
+  user.findById(req.body.id).then(function(use){
+    use.update({firstname:req.body.firstname,lastname:req.body.lastname,phone:req.body.phone,updatedAt: new Date()})
+    res.redirect('/')
+    })
+});
+
+module.exports = router;
